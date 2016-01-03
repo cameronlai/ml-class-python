c02d9738dd251ecd3129aabacf09afc49d3bcdb0
Cameron Lai <cameronlai@users.noreply.github.com>
2016-01-03 10:55:23 +0800

    Add genSkeletons.py script

---
+++ solutions/genSkeletons.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# This scripts generates the skeleton from the solution files
+
+import os
+
+pattern1 = '# ============= YOUR CODE HERE ============='
+pattern2 = '# ==========================================='
+
+for d in os.listdir('.'):
+    if os.path.isdir(d):
+        for f in os.listdir(d):
+            src = d + '/' + f
+            dst = '../skeletons/' + src
+            with open(src, 'r') as f:
+                read_data = f.read()
+            read_data = read_data.splitlines()
+            with open(dst, 'w') as f:
+                code_flag = False
+                for line in read_data:
+                    print(line)
+                    print(code_flag)
+                    if pattern1 in line:
+                        code_flag = True
+                    elif pattern2 in line:
+                        code_flag = False
+
+                    if (code_flag and '#' in line) or not code_flag:
+                        f.write(line + '\n')
