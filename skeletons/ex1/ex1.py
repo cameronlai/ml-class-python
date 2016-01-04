d21b38fd98d5d1fcf1239553fa50c30dca890398
Cameron Lai <cameronlai@users.noreply.github.com>
2016-01-04 23:09:29 +0800

    ex1 Update

--- skeletons/ex1/ex1.py
+++ skeletons/ex1/ex1.py
@@ -7,42 +7,28 @@ def warmUpExercise():
     A = []
     # ============= YOUR CODE HERE =============
     # Instructions: Return the 5x5 identity matrix 
-    A = np.eye(5)
     # ===========================================
     return A
 
 def plotData(x, y):
     # ============= YOUR CODE HERE =============
     # Instructions: Plot the training data into a figure
-    plt.plot(x, y, 'rx', markersize=10)
-    plt.ylabel('Profit in $10,000s')
-    plt.xlabel('Population of City in 10,000s')
     # ===========================================
     
-def computeCost(X, y, theta):
-    m = y.shape[0]
+def computeCost(x, y, theta):
     J = 0
     # ============= YOUR CODE HERE =============
     # Instructions: Compute the cost of a particular choice of theta
     #               You should set J to the cost.
-    Jdiff = np.dot(X, theta) - y
-    print(Jdiff)
-    print(Jdiff.shape)
-    J = np.sum(np.power(Jdiff, 2)) / (2*m)
     # ===========================================
     return J
 
-def gradientDescent(X, y, theta, alpha, num_iters):
+def gradientDescent(x, y, theta, alpha, num_iters):
     m = y.shape[0]
     J_history = np.zeros((num_iters, 1))
     # ============= YOUR CODE HERE =============
     # Instructions: Perform a single gradient step on the parameter vector theta. 
-    for i in range(num_iters):
-        tmpCost = np.sum(np.dot(X, theta), axis=0)
-        theta[0] = theta[0] - alpha *  np.sum(tmpCost - y) / m 
-        theta[1] = theta[1] - alpha *  np.sum((tmpCost - y) * x[1, :]) / m
-        J_history[i] = computeCost(x, y, theta)
-      # ===========================================
+    # ===========================================
     return theta
 
 if __name__ == "__main__":
@@ -66,10 +52,8 @@ if __name__ == "__main__":
 
     x = data[:,0]
     y = data[:,1]
-    m = data.shape[0] # number of training examples
-    y = y.reshape((-1,1)) # create column matrix
+    m = data.shape[0]
 
-    # Note: You have to complete the code in function plotData
     plotData(x,y)
     plt.show(block=False)
 
@@ -79,20 +63,20 @@ if __name__ == "__main__":
 
     print('Running Gradient Descent ...')
 
-    X = np.column_stack((np.ones(m), x))
+    x = np.vstack((np.ones((1,m)), data[:,0]));
     theta = np.zeros((2,1))
 
     iterations = 1500
     alpha = 0.01
 
-    print(computeCost(X, y, theta))
+    print(computeCost(x, y, theta))
 
-    theta = gradientDescent(X, y, theta, alpha, iterations)
+    theta = gradientDescent(x, y, theta, alpha, iterations)
 
     print('Theta found by gradient descent: ');
     print(theta)
 
-    plt.plot(x[1], np.sum(X*theta, axis=0), '-' )
+    plt.plot(x[1], np.sum(x*theta, axis=0), '-' )
     plt.show(block=False)
 
     raw_input('Program paused. Press enter to continue')
