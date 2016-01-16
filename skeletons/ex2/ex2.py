d1e25e12c6c8371105ccb8b7bcebd2b3df630491
Cameron Lai <cameronlai@users.noreply.github.com>
2016-01-17 00:26:38 +0800

    Add ex2 examples

---
+++ skeletons/ex2/ex2.py
@@ -0,0 +1,123 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import fmin
+
+## Machine Learning Online Class - Exercise 2: Logistic Regression
+
+#  Instructions
+#  ------------
+# 
+#  This file contains code that helps you get started on the logistic
+#  regression exercise. You will need to complete the following functions 
+#  in this exericse:
+#
+#     plotData
+#     sigmoid
+#     costFunction
+#     predict
+
+# ==================== All function declaration ====================
+
+def plotData(X, y, labels):
+    # ============= YOUR CODE HERE =============
+    # Instructions: Plot the positive and negative examples on a
+    #               2D plot, using the option 'k+' for the positive
+    #               examples and 'ko' for the negative examples.
+    # ===========================================
+
+def sigmoid(z):
+    g = np.zeros(z.shape)
+    # ============= YOUR CODE HERE =============
+    # Instructions: Compute the sigmoid of each value of z (z can be a matrix, vector or scalar).
+    # ===========================================
+    return g
+
+def costFunction(theta, X, y):
+    m = y.shape[0]
+    J = 0
+    grad = np.zeros(theta.shape)
+    # ============= YOUR CODE HERE =============
+    # Instructions: Compute the cost of a particular choice of theta.
+    #               You should set J to the cost.
+    #               Compute the partial derivatives and set grad to the partial
+    #               derivatives of the cost w.r.t. each parameter in theta
+    #
+    # Note: grad should have the same dimensions as theta
+    # ===========================================
+    return J, grad
+    
+def predict(theta, X):
+    m = X.shape[0]
+    p = np.zeros((m, 1))
+    # ============= YOUR CODE HERE =============
+    # Instructions: Complete the following code to make predictions using
+    #               your learned logistic regression parameters. 
+    #               You should set p to a vector of 0's and 1's
+    # ===========================================
+    return p
+
+if __name__ == "__main__":
+    plt.close('all')
+    plt.ion() # interactive mode
+
+    # ==================== Part 1: Plotting ====================
+
+    print('Plotting data with + indicating (y = 1) examples and o indicating (y = 0) examples.')
+
+    data_file = '../../data/ex2/ex2data1.txt'
+    data = np.loadtxt(data_file, delimiter=',')
+
+    X = data[:,0:2]
+    y = data[:,2]
+    m = data.shape[0] # number of training examples
+    y = y.reshape((-1,1)) # create column matrix
+
+    # Note: You have to complete the code in function plotData
+    plotData(X,y, ['Admitted', 'Not admitted'])
+
+    plt.xlabel('Exam 1 score')
+    plt.ylabel('Exam 2 score')
+    plt.legend()
+
+    raw_input('Program paused. Press enter to continue')
+
+    # =================== Part 2: Compute Cost and Gradient ===================
+
+    m, n = X.shape
+    X = np.column_stack((np.ones(m), X))
+    initial_theta = np.zeros((n+1,1))
+    
+    cost, grad = costFunction(initial_theta, X, y)
+    
+    print('Cost at initial theta (zeros): %f' % cost)
+    print('Gradient at initial theta (zeros):')
+    print(grad)
+
+    raw_input('Program paused. Press enter to continue')
+
+    # =================== Part 3: Optimizing using fminunc ===================
+    
+    fmin_ret = fmin(lambda t : costFunction(t, X, y)[0], initial_theta, maxiter=400, full_output=True)
+    
+    theta = fmin_ret[0]
+    cost = fmin_ret[1]
+
+    print('Cost at theta found by fmin: %f' % cost)
+    print('theta:')
+    print(theta)
+
+    plot_x = np.array([np.min(X[:,1]), np.max(X[:,1])])
+    plot_y = (-1 / theta[2]) * (theta[1] * plot_x + theta[0])
+    plt.plot(plot_x,plot_y, label='Decision Boundary')
+    plt.legend()
+
+    raw_input('Program paused. Press enter to continue')
+
+    # ============= Part 4: Predict and Accuracies =============
+    
+    prob = sigmoid(np.dot([1, 45, 85], theta))
+    print('For a student with scores 45 and 85, we predict an admission probability of %f' % prob)
+
+    p = predict(theta, X)
+
+    print('Train Accuracy: %f' % (np.mean(p == y) * 100))
---
+++ solutions/ex2/ex2_reg.py
@@ -0,0 +1,111 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import fmin_ncg
+from ex2 import *
+
+## Machine Learning Online Class - Exercise 2: Logistic Regression
+
+#  Instructions
+#  ------------
+# 
+#  This file contains code that helps you get started on the second part
+#  of the exercise which covers regularization with logistic regression.
+#
+#  You will need to complete the following functions in this exericse:
+#
+#  mapFeature
+
+# ==================== All function declaration ====================
+
+def mapFeature(X1, X2):
+    degree = 6
+    retval = [np.ones(X1.shape)]
+    for i in range(1, degree+1):
+        for j in range(i+1):
+            retval.append(np.power(X1, (i-j)) * np.power(X2, j))
+    retval = np.transpose(np.array(retval))
+    return retval
+
+def costFunctionReg(theta, X, y, lambda_val):
+    m = y.shape[0]
+    J = 0
+    grad = np.zeros(theta.shape)
+    # ============= YOUR CODE HERE =============
+    # Instructions:  Compute the cost of a particular choice of theta.
+    #               You should set J to the cost.
+    #               Compute the partial derivatives and set grad to the partial
+    #               derivatives of the cost w.r.t. each parameter in theta 
+    J, grad = costFunction(theta, X, y)
+    J = J + lambda_val * np.sum(np.power(theta[1:-1], 2)) / (2 * m)
+    grad[1:] = grad[1:] + lambda_val * theta[1:] / m
+    # ===========================================
+    return J, grad
+
+def plotDecisionBoundary(theta, X, y):
+    u = np.linspace(-1, 1.5, 50)
+    v = np.linspace(-1, 1.5, 50)
+    uu, vv = np.meshgrid(u,v)
+    uv_vals = np.column_stack((uu.ravel(), vv.ravel()))
+    z = np.array([np.dot(mapFeature(uv_value[0], uv_value[1]), theta) for uv_value in uv_vals])
+    z = z.reshape(-1, len(u))
+    plt.contour(uu, vv, z, [0,0], colors='green', label='Decision Boundary')
+
+if __name__ == "__main__":
+    plt.close('all')
+    plt.ion() # interactive mode
+
+    data_file = '../../data/ex2/ex2data2.txt'
+    data = np.loadtxt(data_file, delimiter=',')
+
+    X = data[:,0:2]
+    y = data[:,2]
+    m = data.shape[0] # number of training examples
+    y = y.reshape((-1,1)) # create column matrix
+
+    # Note: You have to complete the code in function plotData
+    plotData(X, y, ['y = 1', 'y = 0'])
+
+    plt.xlabel('Microchip Test 1')
+    plt.xlabel('Microchip Test 2')
+    plt.legend()
+
+    # ==================== Part 1: Regularized Logistic Regression ====================
+
+    # Add Polynomial Features
+    X = mapFeature(X[:,0], X[:,1])
+
+    initial_theta = np.zeros(X.shape[1])
+
+    # Set regularization parameter lambda to 1
+    # As lambda is keyword in Python, it is replaced with lambda_val
+    lambda_val = 1
+
+    cost, grad = costFunctionReg(initial_theta, X, y, lambda_val)
+
+    print('Cost at initial theta (zeros): %f' % cost);
+
+    raw_input('Program paused. Press enter to continue')
+
+    # =================== Part 2: Regularization and Accuracies ===================
+
+    initial_theta = np.zeros((X.shape[1], 1))
+
+    labmda_val = 1
+    
+    fmin_ret = fmin_ncg(lambda t : (costFunctionReg(t, X, y, lambda_val)[0]), initial_theta, lambda t : (costFunctionReg(t, X, y, lambda_val)[1]), maxiter=400, full_output=True)
+    
+    theta = fmin_ret[0]
+    cost = fmin_ret[1]
+
+    print('Cost at theta found by fmin: %f' % cost)
+    print('theta:')
+    print(theta)
+
+    plotDecisionBoundary(theta, X, y)
+    plt.title('lambda = %d' % lambda_val)
+    plt.legend()
+
+    p = predict(theta, X)
+
+    print('Train Accuracy: %f' % (np.mean(p == y) * 100))
+
