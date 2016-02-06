979ebee390762902de147c61885b2abce9cf4a25
Cameron Lai <cameronlai@users.noreply.github.com>
2016-02-06 22:08:30 +0800

    Update ex5 sklearn example, still some problems with validation curve

--- skeletons/ex5/ex5_sklearn.py
+++ skeletons/ex5/ex5_sklearn.py
@@ -5,6 +5,7 @@ from sklearn import linear_model
 from sklearn import cross_validation
 from sklearn.learning_curve import learning_curve, validation_curve
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import make_scorer, mean_squared_error
 
 ## Machine Learning Online Class - Exercise 5: Regularized Linear Regression and Bias-Variance
 
@@ -27,8 +28,8 @@ def polyFeatures(X, p):
 
 def learningCurve(X, y, Xval, yval, lambda_val):
     train_size = X.shape[0]
-    score_train = np.zeros(train_size)
-    score_val = np.zeros(train_size)
+    error_train = np.zeros(train_size)
+    error_val = np.zeros(train_size)
 
     Xin = np.vstack((X, Xval))
     yin = np.vstack((y, yval))
@@ -37,14 +38,14 @@ def learningCurve(X, y, Xval, yval, lambda_val):
     # Instructions: Use sklearn.learning_curve to get learning_curve
                     np.arange(train_size, m)]]) # Fixed generator
     # ===========================================
-    return score_train, score_val
+    return error_train, error_val
 
 def validationCurve(X, y, Xval, yval):
     lambda_vec = np.array([0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3, 10])
 
     train_size = X.shape[0]
-    score_train = np.zeros(train_size)
-    score_val = np.zeros(train_size)
+    error_train = np.zeros(train_size)
+    error_val = np.zeros(train_size)
 
     Xin = np.vstack((X, Xval))
     yin = np.vstack((y, yval))
@@ -53,7 +54,7 @@ def validationCurve(X, y, Xval, yval):
     # Instructions: Use sklearn.learning_curve to get validation_curve
                     np.arange(train_size, m)]]) # Fixed generator
     # ===========================================
-    return lambda_vec, score_train, score_val
+    return lambda_vec, error_train, error_val
 
 if __name__ == "__main__":
     plt.close('all')
@@ -100,20 +101,20 @@ if __name__ == "__main__":
     
     # =================== Part 3: Learning Curve for Linear Regression ===================
 
-    score_train, score_val = learningCurve(X, y, Xval, yval, lambda_val)
+    error_train, error_val = learningCurve(X, y, Xval, yval, lambda_val)
 
     plt.figure()
     plt.title('Learning curve for linear regression')
     plt.legend('Train', 'Cross Validation')
     plt.xlabel('Number of training examples')
-    plt.ylabel('Score')
-    plt.plot(np.arange(1, m+1), score_train, label='Train score')
-    plt.plot(np.arange(1, m+1), score_val, label='Validation score')
+    plt.ylabel('Error')
+    plt.plot(np.arange(1, m+1), error_train, label='Train error')
+    plt.plot(np.arange(1, m+1), error_val, label='Validation error')
     plt.legend()
 
-    print('# Training Examples\tTrain Score\tCross Validation Score')
+    print('# Training Examples\tTrain Error\tCross Validation Error')
     for i in xrange(m):
-        print('  \t%d\t\t%f\t%f' % (i+1, score_train[i], score_val[i]))
+        print('  \t%d\t\t%f\t%f' % (i+1, error_train[i], error_val[i]))
 
     raw_input('Program paused. Press enter to continue')
     
@@ -149,37 +150,37 @@ if __name__ == "__main__":
     X_poly_plot = polyFeatures(X_plot, p)
     plt.plot(X_plot, regr.predict(X_poly_plot), 'b-', label='Linear Regression')
 
-    score_train, score_val = learningCurve(X_poly, y, X_poly_val, yval, lambda_val)
+    error_train, error_val = learningCurve(X_poly, y, X_poly_val, yval, lambda_val)
 
     plt.figure()
     plt.title('Polynomial Regression Fit (lambda = %f)' % lambda_val)
     plt.xlabel('Number of training examples')
-    plt.ylabel('Score')
-    plt.plot(np.arange(1, m+1), score_train, label='Train score')
-    plt.plot(np.arange(1, m+1), score_val, label='Validation score')
+    plt.ylabel('Error')
+    plt.plot(np.arange(1, m+1), error_train, label='Train error')
+    plt.plot(np.arange(1, m+1), error_val, label='Validation error')
     plt.legend()
 
-    print('# Training Examples\tTrain Score\tCross Validation Score')
+    print('# Training Examples\tTrain Error\tCross Validation Error')
     for i in xrange(m):
-        print('  \t%d\t\t%f\t%f' % (i+1, score_train[i], score_val[i]))
+        print('  \t%d\t\t%f\t%f' % (i+1, error_train[i], error_val[i]))
 
     raw_input('Program paused. Press enter to continue')
 
     # =================== Part 8: Validation for Selecting Lambda ===================
 
-    lambda_vec, score_train, score_val = validationCurve(X_poly, y, X_poly_val, yval)
+    lambda_vec, error_train, error_val = validationCurve(X_poly, y, X_poly_val, yval)
 
     plt.close('all')
     plt.figure()
     plt.xlabel('lambda')
-    plt.ylabel('Score')
-    plt.plot(lambda_vec, score_train, label='Train')
-    plt.plot(lambda_vec, score_val, label='Cross Validation')
+    plt.ylabel('Error')
+    plt.plot(lambda_vec, error_train, label='Train')
+    plt.plot(lambda_vec, error_val, label='Cross Validation')
     plt.legend()
 
-    print('# lambda\tTrain Score\tCross Validation Score')
+    print('# lambda\tTrain Error\tCross Validation Error')
     for i in xrange(lambda_vec.shape[0]):
-        print('  \t%d\t\t%f\t%f' % (i+1, score_train[i], score_val[i]))
+        print('  \t%d\t\t%f\t%f' % (i+1, error_train[i], error_val[i]))
 
     raw_input('Program paused. Press enter to continue')
     
--- solutions/ex5/ex5_sklearn.py
+++ solutions/ex5/ex5_sklearn.py
@@ -5,6 +5,7 @@ from sklearn import linear_model
 from sklearn import cross_validation
 from sklearn.learning_curve import learning_curve, validation_curve
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import make_scorer, mean_squared_error
 
 ## Machine Learning Online Class - Exercise 5: Regularized Linear Regression and Bias-Variance
 
@@ -28,45 +29,45 @@ def polyFeatures(X, p):
 
 def learningCurve(X, y, Xval, yval, lambda_val):
     train_size = X.shape[0]
-    score_train = np.zeros(train_size)
-    score_val = np.zeros(train_size)
+    error_train = np.zeros(train_size)
+    error_val = np.zeros(train_size)
 
     Xin = np.vstack((X, Xval))
     yin = np.vstack((y, yval))
     m, n = Xin.shape    
     # ============= YOUR CODE HERE =============
     # Instructions: Use sklearn.learning_curve to get learning_curve
-    regr = linear_model.Ridge(alpha=lambda_val, max_iter=400)
+    regr = linear_model.Ridge(alpha=lambda_val, max_iter=200)
     cv = np.array([[np.arange(train_size),
                     np.arange(train_size, m)]]) # Fixed generator
-    train_sizes, train_scores, val_scores = learning_curve(
-        regr, Xin, yin, cv=cv, train_sizes=np.arange(1, train_size+1))
-    score_train = train_scores
-    score_val = val_scores
+    mse_scorer = make_scorer(mean_squared_error, greater_is_better=True)
+    train_sizes, error_train, error_val = learning_curve(
+        regr, Xin, yin, cv=cv, train_sizes=np.arange(1, train_size+1), 
+        scoring=mse_scorer)
     # ===========================================
-    return score_train, score_val
+    return error_train, error_val
 
 def validationCurve(X, y, Xval, yval):
     lambda_vec = np.array([0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3, 10])
 
     train_size = X.shape[0]
-    score_train = np.zeros(train_size)
-    score_val = np.zeros(train_size)
+    error_train = np.zeros(train_size)
+    error_val = np.zeros(train_size)
 
     Xin = np.vstack((X, Xval))
     yin = np.vstack((y, yval))
     m, n = Xin.shape    
     # ============= YOUR CODE HERE =============
     # Instructions: Use sklearn.learning_curve to get validation_curve
-    regr = linear_model.Ridge(alpha=lambda_val, max_iter=400)
+    regr = linear_model.Ridge(alpha=lambda_val, max_iter=200)
     cv = np.array([[np.arange(train_size),
                     np.arange(train_size, m)]]) # Fixed generator
-    train_scores, val_scores = validation_curve(
-        regr, Xin, yin, param_name='alpha', param_range=lambda_vec, cv=cv)
-    score_train = train_scores
-    score_val = val_scores
+    mse_scorer = make_scorer(mean_squared_error, greater_is_better=True)
+    error_train, error_val = validation_curve(
+        regr, Xin, yin, param_name='alpha', param_range=lambda_vec, cv=cv,
+        scoring=mse_scorer)
     # ===========================================
-    return lambda_vec, score_train, score_val
+    return lambda_vec, error_train, error_val
 
 if __name__ == "__main__":
     plt.close('all')
@@ -100,7 +101,7 @@ if __name__ == "__main__":
     regr = None
     # ============= YOUR CODE HERE =============
     # Instructions: Use sci-kit learn to perform linear regression training
-    regr = linear_model.Ridge(alpha=lambda_val, max_iter=400)
+    regr = linear_model.Ridge(alpha=lambda_val, max_iter=200)
     regr.fit(X, y)
     # ===========================================
     if regr is None:
@@ -115,20 +116,20 @@ if __name__ == "__main__":
     
     # =================== Part 3: Learning Curve for Linear Regression ===================
 
-    score_train, score_val = learningCurve(X, y, Xval, yval, lambda_val)
+    error_train, error_val = learningCurve(X, y, Xval, yval, lambda_val)
 
     plt.figure()
     plt.title('Learning curve for linear regression')
     plt.legend('Train', 'Cross Validation')
     plt.xlabel('Number of training examples')
-    plt.ylabel('Score')
-    plt.plot(np.arange(1, m+1), score_train, label='Train score')
-    plt.plot(np.arange(1, m+1), score_val, label='Validation score')
+    plt.ylabel('Error')
+    plt.plot(np.arange(1, m+1), error_train, label='Train error')
+    plt.plot(np.arange(1, m+1), error_val, label='Validation error')
     plt.legend()
 
-    print('# Training Examples\tTrain Score\tCross Validation Score')
+    print('# Training Examples\tTrain Error\tCross Validation Error')
     for i in xrange(m):
-        print('  \t%d\t\t%f\t%f' % (i+1, score_train[i], score_val[i]))
+        print('  \t%d\t\t%f\t%f' % (i+1, error_train[i], error_val[i]))
 
     raw_input('Program paused. Press enter to continue')
     
@@ -164,37 +165,37 @@ if __name__ == "__main__":
     X_poly_plot = polyFeatures(X_plot, p)
     plt.plot(X_plot, regr.predict(X_poly_plot), 'b-', label='Linear Regression')
 
-    score_train, score_val = learningCurve(X_poly, y, X_poly_val, yval, lambda_val)
+    error_train, error_val = learningCurve(X_poly, y, X_poly_val, yval, lambda_val)
 
     plt.figure()
     plt.title('Polynomial Regression Fit (lambda = %f)' % lambda_val)
     plt.xlabel('Number of training examples')
-    plt.ylabel('Score')
-    plt.plot(np.arange(1, m+1), score_train, label='Train score')
-    plt.plot(np.arange(1, m+1), score_val, label='Validation score')
+    plt.ylabel('Error')
+    plt.plot(np.arange(1, m+1), error_train, label='Train error')
+    plt.plot(np.arange(1, m+1), error_val, label='Validation error')
     plt.legend()
 
-    print('# Training Examples\tTrain Score\tCross Validation Score')
+    print('# Training Examples\tTrain Error\tCross Validation Error')
     for i in xrange(m):
-        print('  \t%d\t\t%f\t%f' % (i+1, score_train[i], score_val[i]))
+        print('  \t%d\t\t%f\t%f' % (i+1, error_train[i], error_val[i]))
 
     raw_input('Program paused. Press enter to continue')
 
     # =================== Part 8: Validation for Selecting Lambda ===================
 
-    lambda_vec, score_train, score_val = validationCurve(X_poly, y, X_poly_val, yval)
+    lambda_vec, error_train, error_val = validationCurve(X_poly, y, X_poly_val, yval)
 
     plt.close('all')
     plt.figure()
     plt.xlabel('lambda')
-    plt.ylabel('Score')
-    plt.plot(lambda_vec, score_train, label='Train')
-    plt.plot(lambda_vec, score_val, label='Cross Validation')
+    plt.ylabel('Error')
+    plt.plot(lambda_vec, error_train, label='Train')
+    plt.plot(lambda_vec, error_val, label='Cross Validation')
     plt.legend()
 
-    print('# lambda\tTrain Score\tCross Validation Score')
+    print('# lambda\tTrain Error\tCross Validation Error')
     for i in xrange(lambda_vec.shape[0]):
-        print('  \t%d\t\t%f\t%f' % (i+1, score_train[i], score_val[i]))
+        print('  \t%d\t\t%f\t%f' % (i+1, error_train[i], error_val[i]))
 
     raw_input('Program paused. Press enter to continue')
     
