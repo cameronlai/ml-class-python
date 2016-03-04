0f9722260abeca1aa17e3a941bda4614a14412c0
Cameron Lai <cameronlai@users.noreply.github.com>
2016-03-05 01:54:29 +0800

    Add ex8_cofi file

---
+++ solutions/ex8/ex8_cofi.py
@@ -0,0 +1,225 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io as sio
+from scipy import optimize
+from ex8_utility import *
+
+## Machine Learning Online Class - Exercise 8: Anomaly Detection and Collaborative Filtering
+
+#  Instructions
+#  ------------
+# 
+#  This file contains code that helps you get started on the
+#  exercise. You will need to complete the following functions 
+#  in this exericse:
+#
+#     cofiCostFunc
+#     cofiGradFunc
+#
+
+# ==================== All function declaration ====================
+
+def cofiCostFunc(params, Y, R, num_users, num_movies, num_features, lambda_val):
+    X = np.reshape(params[:num_movies*num_features], (num_movies, num_features))
+    Theta = np.reshape(params[num_movies*num_features:], (num_users, num_features))
+
+    J = 0
+    # ============= YOUR CODE HERE =============
+    # Instructions: Compute the cost function and gradient for collaborative
+    #               filtering. Concretely, you should first implement the cost
+    #               function (without regularization) and make sure it is
+    #               matches our costs. After that, you should implement the 
+    #               gradient and use the checkCostFunction routine to check
+    #               that the gradient is correct. Finally, you should implement
+    #               regularization.
+    diff = X.dot(Theta.transpose()) - Y
+    
+    J = np.power(diff, 2) / 2
+    J = np.sum(J * R)
+    J += lambda_val * np.sum(np.power(Theta, 2)) / 2
+    J += lambda_val * np.sum(np.power(X, 2)) / 2
+    # ===========================================
+    return J
+
+def cofiGradFunc(params, Y, R, num_users, num_movies, num_features, lambda_val):
+    X = np.reshape(params[:num_movies*num_features], (num_movies, num_features))
+    Theta = np.reshape(params[num_movies*num_features:], (num_users, num_features))
+
+    X_grad = np.zeros(X.shape)
+    Theta_grad = np.zeros(Theta.shape)
+    # ============= YOUR CODE HERE =============
+    # Instructions: Compute the cost function and gradient for collaborative
+    #               filtering. Concretely, you should first implement the cost
+    #               function (without regularization) and make sure it is
+    #               matches our costs. After that, you should implement the 
+    #               gradient and use the checkCostFunction routine to check
+    #               that the gradient is correct. Finally, you should implement
+    #               regularization.
+    diff = X.dot(Theta.transpose()) - Y
+    
+    X_grad = (diff * R).dot(Theta)
+    X_grad += lambda_val * X
+
+    Theta_grad = (diff * R).transpose().dot(X)
+    Theta_grad += lambda_val * Theta           
+    # ===========================================
+    grad = np.hstack((X_grad.ravel(), Theta_grad.ravel()))
+    return grad
+
+if __name__ == "__main__":
+    plt.close('all')
+    plt.ion() # interactive mode
+
+    # ==================== Part 1: Load movie ratings dataset ====================
+    
+    print('Loading movie ratings dataset.')
+
+    data_file = '../../data/ex8/ex8_movies.mat'
+    mat_content = sio.loadmat(data_file)
+
+    Y = mat_content['Y']
+    R = mat_content['R']
+
+    plt.imshow(Y)
+    plt.ylabel('Movies')
+    plt.xlabel('Users')
+
+    raw_input('Program paused. Press enter to continue')
+
+    # =================== Part 2: Collaborative Filtering Cost Function ===================
+
+    data_file = '../../data/ex8/ex8_movieParams.mat'
+    mat_content = sio.loadmat(data_file)
+
+    X = mat_content['X']
+    Theta = mat_content['Theta']
+
+    # Reduce the data set size so that this runs faster
+    num_users = 4
+    num_movies = 5
+    num_features = 3
+
+    X = X[:num_movies, :num_features]
+    Theta = Theta[:num_users, :num_features]
+    Y = Y[:num_movies, :num_users]
+    R = R[:num_movies, :num_users]
+
+    params = np.hstack((X.ravel(), Theta.ravel()))
+    J = cofiCostFunc(params, Y, R, num_users, num_movies, num_features, 0)
+    
+    print('Cost at loaded parameters: %f' % J)
+    print('(this value should be about 22.22)')
+
+
+    raw_input('Program paused. Press enter to continue')
+
+    # =================== Part 3: Collaborative Filtering Gradient ===================
+
+    print('Checking Gradients (without regularization) ... ')
+    
+    checkCostFunction(0)
+    
+    raw_input('Program paused. Press enter to continue')
+
+    # =================== Part 4: Collaborative Filtering Cost Regularization ===================
+    
+    params = np.hstack((X.ravel(), Theta.ravel()))
+    J = cofiCostFunc(params, Y, R, num_users, num_movies, num_features, 1.5)
+           
+    print('Cost at loaded parameters (lambda = 1.5): %f' % J)
+    print('this value should be about 31.34)')
+    
+    raw_input('Program paused. Press enter to continue')
+
+    # =================== Part 5: Collaborative Filtering Gradient Regularization ===================
+
+    print('Checking Gradients (with regularization) ... ')
+    
+    checkCostFunction(1.5)
+
+    raw_input('Program paused. Press enter to continue')
+
+    # =================== Part 6: Entering ratings for a new user ===================
+
+    movieList = loadMovieList()
+
+    my_ratings = np.zeros((1682, 1))
+
+    my_ratings[0] = 4
+    my_ratings[97] = 2
+
+    my_ratings[6] = 3;
+    my_ratings[11]= 5;
+    my_ratings[53] = 4;
+    my_ratings[63]= 5;
+    my_ratings[65]= 3;
+    my_ratings[68] = 5;
+    my_ratings[182] = 4;
+    my_ratings[225] = 5;
+    my_ratings[354]= 5;
+
+    print('New user ratings')
+    for i in xrange(len(my_ratings)):
+        if my_ratings[i] > 0:
+            print('Rated %d for %s' % (my_ratings[i], movieList[i]))
+
+    raw_input('Program paused. Press enter to continue')
+
+    # =================== Part 7: Learning Movie Ratings ===================
+
+    print('Training collaborative filtering...')
+
+    data_file = '../../data/ex8/ex8_movies.mat'
+    mat_content = sio.loadmat(data_file)
+
+    Y = mat_content['Y']
+    R = mat_content['R']
+
+    Y = np.column_stack((my_ratings, Y))
+    R = np.column_stack(((my_ratings != 0), R))
+    
+    Ynorm, Ymean = normalizeRatings(Y, R)
+
+    num_users = Y.shape[1]
+    num_movies = Y.shape[0]
+    num_features = 10
+
+    X = np.random.random((num_movies, num_features))
+    Theta = np.random.random((num_users, num_features))
+
+    initial_parameters = np.hstack((X.ravel(), Theta.ravel()))
+    
+    lambda_val = 10
+
+    costFunc = lambda t : cofiCostFunc(t, Y, R, num_users, num_movies, num_features, lambda_val)
+    gradFunc = lambda t : cofiGradFunc(t, Y, R, num_users, num_movies, num_features, lambda_val)
+
+    fmin_ret = optimize.fmin_cg(costFunc, initial_parameters, gradFunc, maxiter=100, full_output=True)
+
+    params = fmin_ret[0]
+    X = np.reshape(params[:num_movies*num_features], (num_movies, num_features))
+    Theta = np.reshape(params[num_movies*num_features:], (num_users, num_features))
+
+    print('Recommender system learning completed.')
+    
+    raw_input('Program paused. Press enter to continue')
+
+    # =================== Part 8: Recommendation for you ===================
+
+    p = X.dot(Theta.transpose())
+    my_predictions = p[:,0] + Ymean.ravel()
+
+    idx = np.argsort(my_predictions)[::-1]
+    
+    print('Top recommendations for you:')
+    for i in xrange(10):
+        j = idx[i]
+        print('Predicting rating %.1f for movie %s' %  (my_predictions[j], movieList[j]))
+
+    print('\nOriginal ratings provided:');
+    for i in xrange(10):
+        if my_ratings[i] > 0:
+            print('Rated %d for %s\n' % (my_ratings[i], movieList[i]))
+
+    raw_input('Program paused. Press enter to continue')    
+    plt.close('all')
--- solutions/ex8/ex8_utility.py
+++ solutions/ex8/ex8_utility.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from ex8_cofi import *
 
 def multivariateGaussian(X, mu, Sigma2):
     k = mu.size
@@ -28,5 +29,84 @@ def visualizeFit(X, mu, Sigma2):
     level = np.power(10.0, np.arange(-20, 0, 3))
     plt.contour(X1, X2, Z, level)
 
+def visualizeFit_sklearn(clf, threshold):
+    XGridVal = np.arange(0, 35, 0.5)
+    X1, X2 = np.meshgrid(XGridVal, XGridVal)
+    Z = clf.decision_function(np.c_[X1.ravel(), X2.ravel()])
+    Z = Z.reshape(X1.shape)
+    plt.title('Outlier detection with elliptical envelope')
+    plt.contourf(X1, X2, Z, levels=np.linspace(Z.min(), threshold, 7), cmap=plt.cm.Blues_r)
 
+def computeNumericalGradient(J, theta):
+    numgrad = np.zeros(theta.size)
+    perturb = np.zeros(theta.size)
+    e = 1e-4
+    for p in range(theta.size):
+        perturb[p] = e
+        tmpPerturb = perturb.reshape(theta.shape)
+        loss1 = J(theta - tmpPerturb)
+        loss2 = J(theta + tmpPerturb)
+        numgrad[p] = (loss2 - loss1) / (2*e)
+        perturb[p] = 0       
+    numgrad = numgrad.reshape(theta.shape)
+    return numgrad
 
+def checkCostFunction(lambda_val):
+    if lambda_val is None:
+        lambda_val = 0
+
+    X_t = np.random.random((4,3))
+    Theta_t = np.random.random((5,3))
+
+    Y = X_t.dot(Theta_t.transpose())
+    Y[np.random.random(Y.shape) > 0.5] = 0
+    R = np.zeros(Y.shape)
+    R[Y != 0] = 1
+
+    X = np.random.random(X_t.shape)
+    Theta = np.random.random(Theta_t.shape)
+    num_users = Y.shape[1]
+    num_movies = Y.shape[0]
+    num_features = Theta_t.shape[1]
+
+    params = np.hstack((X.ravel(), Theta.ravel()))
+
+    costFunc = lambda t : cofiCostFunc(t, Y, R, num_users, num_movies, num_features, lambda_val)
+
+    gradFunc = lambda t : cofiGradFunc(t, Y, R, num_users, num_movies, num_features, lambda_val)
+
+    cost = costFunc(params)
+    grad = gradFunc(params)
+    numgrad = computeNumericalGradient(costFunc, params)
+
+    display_grad = np.column_stack((numgrad.ravel(), grad.ravel()))
+    print(display_grad)
+    print('The above two columns you get should be very similar')
+    print('(Left-Your Numerical Gradient, Right-Analytical Gradient)')
+
+    diff = np.linalg.norm(numgrad - grad) / np.linalg.norm(numgrad + grad)
+    print('If your backpropagation implementation is correct, then')
+    print('the relative difference will be small (less than 1e-9).')
+    print('Relative Difference: %f' % diff)
+
+def loadMovieList():
+    movieList = []
+    data_file = '../../data/ex8/movie_ids.txt'
+    f = open(data_file)
+    n = 1682
+    for i in xrange(n):
+        line = f.readline()       
+        idx, movieName = line.strip().split(' ', 1)
+        movieList.append(movieName)
+    f.close()
+    return movieList
+
+def normalizeRatings(Y, R):
+    m, n = Y.shape
+    Ymean = np.zeros((m,1))
+    Ynorm = np.zeros(Y.shape)
+    for i in xrange(m):
+        idx = np.where(R[i, :] == 1)
+        Ymean[i] = np.mean(Y[i, idx])
+        Ynorm[i, idx] = Y[i, idx] - Ymean[i]
+    return Ynorm, Ymean
